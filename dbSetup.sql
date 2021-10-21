4d9bea63965e39b913bc66908598dce4f7e17243
NickSondrup <nicholas.sondrup@gmail.com>
2021-10-20 22:50:43 -0600

    practice with mySQL

--- dbSetup.sql
+++ dbSetup.sql
@@ -6,5 +6,28 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS castles(
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT comment 'primary key',
+  name varchar(255) comment 'castles name',
+  kingdom varchar(255) comment 'kingdom name'
+) default charset utf8 comment'';
+CREATE TABLE IF NOT EXISTS knights(
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT comment 'primary key',
+  knightName varchar(255) comment 'Knights name',
+  castleId int NOT NULL,
+  FOREIGN KEY (castleId) REFERENCES castles(id) ON DELETE CASCADE
+)default charset utf8 comment '';
+
+
+INSERT INTO knights (knightName, castleId)
+VALUES("shining armor", 4);
+
+SELECT * FROM knights;
+
+SELECT
+*
+FROM knights k 
+JOIN castles c 
+ON c.id = k.castleId;
 
 
